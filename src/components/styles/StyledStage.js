951f52a9e17051228075ecd97115e826891d48c0
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-10-25 17:05:34 +0530

    fix: cell width is not fixed

--- src/components/styles/StyledStage.js
+++ src/components/styles/StyledStage.js
@@ -3,11 +3,12 @@ import styled from 'styled-components'
 export const StyledStage = styled.div`
   display: grid;
   grid-template-rows: repeat(
-    ${(props) => props.height},
-    calc(25vw / ${(props) => props.width})
+    ${props => props.height},
+    calc(25vw / ${props => props.width})
   );
-  grid-template-columns: repeat(${(props) => props.width}, 1fr);
+  grid-template-columns: repeat(${props => props.width}, 1fr);
   grid-gap: 1px;
+  width: 100%;
   border: 2px solid #333;
   max-width: 25vw;
   background: #111;
