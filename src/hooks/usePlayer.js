ed6ff979cd7d3d6b2b6bcd5b7c74b942da7a2764
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-10-25 17:06:52 +0530

    chore: add rotate function

--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,6 +1,6 @@
 import { useState, useCallback } from 'react'
 import { generateRandomTetromino } from '../utils/tetrominos'
-import { STAGE_WIDTH } from '../utils/gameHelper'
+import { STAGE_WIDTH, checkCollision } from '../utils/gameHelper'
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState({
@@ -8,6 +8,33 @@ export const usePlayer = () => {
     tetromino: generateRandomTetromino().shape,
     collided: false,
   })
+
+  const rotate = (matrix, dir) => {
+    const rotatedTetro = matrix.map((_, index) => matrix.map(col => col[index]))
+
+    if (dir > 0) {
+      return rotatedTetro.map(row => row.reverse())
+    }
+    return rotatedTetro.reverse()
+  }
+
+  const playerRotate = (stage, dir) => {
+    const clonedPlayer = JSON.parse(JSON.stringify(player))
+    clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir)
+
+    const pos = clonedPlayer.pos.x
+    let offset = 1
+    while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+      clonedPlayer.pos.x += offset
+      offset = -(offset + (offset > 0 ? 1 : -1))
+      if (offset > clonedPlayer.tetromino[0].length) {
+        rotate(clonedPlayer.tetromino, -dir)
+        clonedPlayer.pos.x = pos
+        return
+      }
+    }
+    setPlayer(clonedPlayer)
+  }
 
   const updatePlayerPos = ({ x, y, collided }) => {
     setPlayer(prev => ({
@@ -17,14 +44,13 @@ export const usePlayer = () => {
     }))
   }
 
-  const resetPlayer = useCallback(
+  const resetPlayer = useCallback(() => {
     setPlayer({
-      pos: { x: STAGE_WIDTH / 2 - 2 },
+      pos: { x: STAGE_WIDTH / 2 - 2, y: 0 },
       tetromino: generateRandomTetromino().shape,
       collided: false,
-    }),
-    []
-  )
+    })
+  }, [])
 
-  return [player, updatePlayerPos, resetPlayer]
+  return [player, updatePlayerPos, playerRotate, resetPlayer]
 }
