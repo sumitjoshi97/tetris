6f8df75a43765c3d7d10e129bb2b6971e4500757
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-10-25 17:44:37 +0530

    chore: prettify code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from 'react'
 import Tetris from './components/Tetris'
 
 const App = () => (
   <div className="app">
-    <Tetris/>
+    <Tetris />
   </div>
-) 
+)
 
-export default App;
+export default App
