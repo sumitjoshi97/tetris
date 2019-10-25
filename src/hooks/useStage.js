4265b026b75d888bb64f8adc7f870227de574c9b
sumitjoshi97 <sumitjoshi479@gmail.com>
2019-10-25 17:07:57 +0530

    chore: add function to clear tetro-fill row

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -4,9 +4,23 @@ import { createStage } from '../utils/gameHelper'
 
 export const useStage = (player, resetPlayer) => {
   const [stage, setStage] = useState(createStage())
+  const [rowsCleared, setRowsCleared] = useState(0)
 
   useEffect(
     () => {
+      setRowsCleared(0)
+
+      const sweepRows = newStage =>
+        newStage.reduce((ack, row) => {
+          if (row.findIndex(cell => cell[0] === 0) === -1) {
+            setRowsCleared(prev => prev + 1)
+            ack.unshift(new Array(newStage[0].length).fill([0, 'clear']))
+            return ack
+          }
+          ack.push(row)
+          return ack
+        }, [])
+
       const udpateStage = prevStage => {
         const newStage = prevStage.map(row =>
           row.map(cell => (cell[1] === 'clear' ? [0, 'clear'] : cell))
@@ -22,13 +36,19 @@ export const useStage = (player, resetPlayer) => {
             }
           })
         })
+
+        if (player.collided) {
+          resetPlayer()
+          return sweepRows(newStage)
+        }
+
         return newStage
       }
 
       setStage(prev => udpateStage(prev))
     },
-    [player.collided, player.pos.x, player.pos.y, player.tetromino]
+    [player.collided, player.pos.x, player.pos.y, player.tetromino, resetPlayer]
   )
 
-  return [stage, setStage]
+  return [stage, setStage, rowsCleared]
 }
